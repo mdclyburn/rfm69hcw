1428467c367a4939b92483c4c73ae11222d953a0
Marshall Clyburn <mardev2013@gmail.com>
2019-04-05 16:09:27 -0400

    Add addressing define and clean up defines

--- src/rfm69_configuration.h
+++ src/rfm69_configuration.h
@@ -4,6 +4,8 @@
 
 /** Configuration
  *
+ * RFM_CONFIG_NODEADDRESS
+ *     8-bit address the node listens for packets sent to.
  * RFM_CONFIG_BITRATE
  *     Rate of data transmission.
  *     Defaults to 4.8kbps.
@@ -44,6 +46,8 @@
 
 #define RFM_CONFIG_ENCRYPTIONKEY { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 }
 
+#define RFM_CONFIG_NODEADDRESS 0x01
+
 // #define RFM_CONFIG_PACKETFIXED
 
 // #define RFM_CONFIG_PACKETSIZE 32
@@ -61,7 +65,7 @@
 /** Features
  *
  * RFM_FEATURE_ADDRESSING
- *     Not implemented.
+ *     Use a node address and send packets with addresses.
  * RFM_FEATURE_CRC
  *     Not implemented.
  * RFM_FEATURE_ENCRYPTION (~ +160 bytes)
@@ -79,6 +83,8 @@
  *     Supplies rfm_temperature() to read the CMOS temperature sensor.
  */
 
+#define RFM_FEATURE_ADDRESSING
+
 #define RFM_FEATURE_ENCRYPTION
 
 // #define RFM_FEATURE_LISTEN
@@ -94,50 +100,45 @@
 // ===== Autoconfiguration =====================================================
 // =============================================================================
 
+/* RFM_CONFIG_BITRATE: Defaults to 4.8kbps.
+ */
+#ifndef RFM_CONFIG_BITRATE
+  #define RFM_CONFIG_BITRATE 0x1A0B
+#endif
+
 /* RFM_CONFIG_PACKETVARIABLE: set max payload received size
  */
-#ifdef RFM_CONFIG_PACKETVARIABLE
-
-#ifndef RFM_CONFIG_PACKETSIZE
+#if defined(RFM_CONFIG_PACKETVARIABLE) && !defined(RFM_CONFIG_PACKETSIZE)
 
 // Max receive size is 255, regardless of addressing.
-#ifndef RFM_FEATURE_ENCRYPTION
-#define RFM_CONFIG_PACKETSIZE 255
-#endif
+  #ifndef RFM_FEATURE_ENCRYPTION
+    #define RFM_CONFIG_PACKETSIZE 255
+  #endif
 
 // Max receive size is 65 bytes without addressing: length byte + message (1 + 64)
 // Max receive size is 50 bytes with addressing: length byte + address byte + message (1 + 1 + 48)
-#ifdef RFM_FEATURE_ENCRYPTION
-#ifndef RFM_FEATURE_ADDRESSING
-#define RFM_CONFIG_PACKETSIZE 65
-#endif
-#else
-#define RFM_CONFIG_PACKETSIZE 50
-#endif
-
-#endif
-
+  #ifdef RFM_FEATURE_ENCRYPTION
+    #ifndef RFM_FEATURE_ADDRESSING
+      #define RFM_CONFIG_PACKETSIZE 65
+    #else
+      #define RFM_CONFIG_PACKETSIZE 50
+    #endif
+  #endif // #ifdef RFM_FEATURE_ENCRYPTION
 #endif
 
 // ===== Sanity Checks =========================================================
 // =============================================================================
 
-/* RFM_CONFIG_BITRATE: Defaults to 4.8kbps.
+/* RFM_FEATURE_ADDRESSING: An address is specified.
  */
-#ifndef RFM_CONFIG_BITRATE
-
-#define RFM_CONFIG_BITRATE 0x1A0B
-
+#if defined(RFM_FEATURE_ADDRESSING) && !defined(RFM_CONFIG_NODEADDRESS)
+  #error To use addressing: specify an address with RFM_CONFIG_NODEADDRESS.
 #endif
 
 /* RFM_FEATURE_ENCRYPTION: An encryption key must be supplied when encryption is enabled.
  */
-#ifdef RFM_FEATURE_ENCRYPTION
-
-#ifndef RFM_CONFIG_ENCRYPTIONKEY
-#error To use packet encryption: specify an encryption key up to 16 bytes in length with RFM_CONFIG_ENCRYPTIONKEY.
-#endif
-
+#if defined(RFM_FEATURE_ENCRYPTION) && !defined(RFM_CONFIG_ENCRYPTIONKEY)
+  #error To use packet encryption: specify an encryption key up to 16 bytes in length with RFM_CONFIG_ENCRYPTIONKEY.
 #endif
 
 /* RFM_CONFIG_PACKETFIXED:
@@ -145,76 +146,53 @@
  * - RFM_CONFIG_PACKETSIZE is defined.
  */
 #ifdef RFM_CONFIG_PACKETFIXED
+  #ifdef RFM_CONFIG_PACKETVARIABLE
+    #error Both fixed- and variable-size packets cannot be used at the same time.
+  #endif
 
-#ifdef RFM_CONFIG_PACKETVARIABLE
-#error Both fixed- and variable-size packets cannot be used at the same time.
+  #ifndef RFM_CONFIG_PACKETSIZE
+    #error One of either fixed or variable-length packets must be specified with RFM_CONFIG_PACKETFIXED or RFM_CONFIG_PACKETVARIABLE.
+  #endif
 #endif
 
-#endif
-
-/* RFM_CONFIG_PACKETVARIABLE: check the maximum received payload size.
+/* RFM_CONFIG_PACKETVARIABLE: check the maximum received payload size (max 255 bytes).
  */
 #ifdef RFM_CONFIG_PACKETVARIABLE
+  #if !defined(RFM_FEATURE_ENCRYPTION) && RFM_CONFIG_PACKETSIZE > 255
+    #error Packet size cannot be greater than 255 bytes.
+  #endif
 
-// Max receive size is 255, regardless of addressing.
-#ifndef RFM_FEATURE_ENCRYPTION
-#if RFM_CONFIG_PACKETSIZE > 255
-#error Packet size cannot be greater than 255 bytes.
-#endif
-#endif
+// Max receive size is 50 bytes with addressing: length byte + address byte + message (1 + 1 + 48)
+  #if defined(RFM_FEATURE_ENCRYPTION) && defined(RFM_FEATURE_ADDRESSING) && RFM_CONFIG_PACKETSIZE > 50
+    #error Packet size cannot be greater than 50 bytes.
+  #endif
 
 // Max receive size is 65 bytes without addressing: length byte + message (1 + 64)
-// Max receive size is 50 bytes with addressing: length byte + address byte + message (1 + 1 + 48)
-#ifdef RFM_FEATURE_ENCRYPTION
-#ifdef RFM_FEATURE_ADDRESSING
-
-#if RFM_CONFIG_PACKETSIZE > 50
-#error Packet size cannot be greater than 50 bytes.
-#endif
-
-#else // not using addressing
-
-#if RFM_CONFIG_PACKETSIZE > 65
-#error Packet size cannot be greater than 65 bytes.
-#endif
-
-#endif // addressing
+  #if defined(RFM_FEATURE_ENCRYPTION) && !defined (RFM_FEATURE_ADDRESSING) && RFM_CONFIG_PACKETSIZE > 65
+     #error Packet size cannot be greater than 65 bytes.
+  #endif
 #endif // encryption
 
-#endif
-
-/* RFM_CONFIG_PACKETFIXED, RFM_CONFIG_PACKETVARIABLE: a packet mode is set
+/* RFM_CONFIGPACKETFIXED: packet size must be specified
  */
-#ifndef RFM_CONFIG_PACKETFIXED
-#ifndef RFM_CONFIG_PACKETVARIABLE
-#error One of either fixed or variable-length packets must be specified with RFM_CONFIG_PACKETFIXED or RFM_CONFIG_PACKETVARIABLE.
-#endif
-#endif
-
-#ifndef RFM_CONFIG_PACKETSIZE
-#error To use fixed packet size: the packet size must be defined with RFM_CONFIG_PACKETSIZE.
+#if defined(RFM_CONFIG_PACKETFIXED) && !defined(RFM_CONFIG_PACKETSIZE)
+  #error To use fixed packet size: the packet size must be defined with RFM_CONFIG_PACKETSIZE.
 #endif
 
 /* RFM_FEATURE_SYNCWORD: RFM_CONFIG_SYNCWORD and RFM_CONFIG_SYNCWORDLENGTH must be supplied.
  */
 #ifdef RFM_FEATURE_SYNCWORD
+  #ifndef RFM_CONFIG_SYNCWORD
+    #error To use sync word: a sync word must be specified with RFM_CONFIG_SYNCWORD (remember to set the length also!).
+  #endif
 
-#ifndef RFM_CONFIG_SYNCWORD
-#error To use sync word: a sync word must be specified with RFM_CONFIG_SYNCWORD (remember to set the length also!).
-#endif
-
-#ifndef RFM_CONFIG_SYNCWORDLENGTH
-#error To use sync word: the length of the sync word length must be specified with RFM_CONFIG_SYNCWORDLENGTH (length should match array length minus one).
-#endif
-
+  #ifndef RFM_CONFIG_SYNCWORDLENGTH
+    #error To use sync word: the length of the sync word length must be specified with RFM_CONFIG_SYNCWORDLENGTH (length should match array length minus one).
+  #endif
 #endif
 
 /* RFM_FEATURE_RESET: RFM_CONFIG_PINRESET must be supplied.
  */
-#ifdef RFM_FEATURE_RESET
-
-#ifndef RFM_CONFIG_PINRESET
-#error To use the reset pin: a pin number must be specified with RFM_CONFIG_PINRESET.
-#endif
-
+#if defined(RFM_FEATURE_RESET) && !defined(RFM_CONFIG_PINRESET)
+  #error To use the reset pin: a pin number must be specified with RFM_CONFIG_PINRESET.
 #endif
